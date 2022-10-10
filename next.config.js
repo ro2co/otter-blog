f06b18ea817c8d651908fa88df5dc53b1ce1b145
ro2co <272157218@qq.com>
2022-10-10 11:41:25 +0800

    add mdx dependency

--- next.config.js
+++ next.config.js
@@ -1,8 +1,12 @@
 /** @type {import('next').NextConfig} */
+const withMDX = require('@next/mdx')({
+  extension: /\.mdx?$/,
+})
 
-const nextConfig = {
+// next.config.js
+const nextConfig= withMDX({
+  pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdx'],
   reactStrictMode: true,
   swcMinify: true,
-}
-
+})
 module.exports = nextConfig
