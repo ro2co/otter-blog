f2d1f31aab4adc531e6100e9883b36e759f68d02
ro2co <272157218@qq.com>
2023-04-16 19:30:16 +0800

    add disqus-react

---
+++ config.js
@@ -0,0 +1,24 @@
+import { theme } from "@chakra-ui/react";
+
+export default  {
+  site_name: "road2code",
+  site_url: "https://road2code.me",
+  site_logo: "",
+  author: "ro2co",
+  email: "272157218@qq.com",
+  repo: "",
+  sidebar_position: "right",
+  posts_num_per_page: 6,
+  github: "https://github.com/ro2co",
+  analytics: {
+
+  },
+  comment: {
+
+  },
+  theme: {
+    sidebar: true,
+    top_nav: "fixed",
+    
+  }
+}
--- layouts/index.tsx
+++ layouts/index.tsx
@@ -1,5 +1,6 @@
 import Header from '@/components/header'
 import Footer from '@/components/footer'
+import Setting from '@/components/setting'
 const Layout = ({children} :any) =>{
   return (
     <>
