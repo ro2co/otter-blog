6ede2cab546e560533154ec0173f23a83d227ca9
ro2co <272157218@qq.com>
2023-05-22 21:13:39 +0800

    change some markdown detail

--- data/pages/about.md
+++ data/pages/about.md
@@ -1,15 +1,10 @@
 ---
-name: Tails Azimuth
-avatar: /static/images/avatar.png
-occupation: Professor of Atmospheric Science
-company: Stanford University
-email: address@yoursite.com
-twitter: https://twitter.com/Twitter
-linkedin: https://www.linkedin.com
-github: https://github.com
 ---
 
-Hello , 我是road2code。我是一位热爱技术开发的工程师，我喜欢探索新技术和工具，并且乐于将它们应用到我的项目中，以提高用户体验和开发效率。我使用基于 chatgpt api 制作的ai 工具网站已上线多时，并为很多用户提供稳定而快速的服务，如有需要注册，请联系我（微信：eXdjbTY1Mjc=）
-另一方面我也喜欢研究事物的本质原理，比如我在学习南京大学蒋岩炎的操作系统原理课和 go 语言。
+Hello , 我是road2code。一位热爱技术的工程师，我喜欢探索新技术和工具，并且乐于将它们应用到我的项目中，以提高用户体验和开发效率。我使用基于 chatgpt api 制作的ai 工具网站已上线多时，并为很多用户提供稳定而快速的服务，如有需要注册，请联系我（微信：eXdjbTY1Mjc=）
+
+另一方面我也喜欢研究事物的本质原理，比如我在学习南京大学蒋岩炎的操作系统原理课程和 go 语言。
+
+
 这个博客是自己使用 Nextjs 和 Chakra-ui 开发，移动端仍在完善中，谢谢。
 
--- pages/about.tsx
+++ pages/about.tsx
@@ -1,4 +1,4 @@
-import {Container} from "@chakra-ui/react"
+import {Container, Box, Flex} from "@chakra-ui/react"
 import { getFileBySlug } from "@/libs/utils"
 
 export async function getStaticProps() {
@@ -6,8 +6,15 @@ export async function getStaticProps() {
   return {props: {aboutData}}
 }
 const AboutPage = ({aboutData}: any) => (
-  <Container p="0">
-    about {aboutData.content}
+  <Container p="0" lineHeight="2.4em">
+    <Flex minH="300px">
+      <Flex w="32%" alignItems="center" justifyContent="right" fontWeight="bold" fontSize="3em" >
+        <Box borderRight="2px solid #666" pr="0.5em">About</Box>
+      </Flex>
+      <Box w="68%" flexWrap="wrap" pl="2em">
+        {aboutData.content}
+      </Box>
+    </Flex>
   </Container>
 )
 export default AboutPage
